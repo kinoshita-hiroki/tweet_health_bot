7f39dfe00028f0e1dd168f2b38cb1f5dc268ee33
kinoshita-hiroki <kinohiro1031@gmail.com>
2021-08-14 17:11:53 +0900

    完成

--- tweet_health_bot.sh
+++ tweet_health_bot.sh
@@ -1,3 +1,15 @@
-month=date '+%m'
-echo "${s}"
-# twurl -d 'status=Test tweet using the POST statuses/update endpoint' /1.1/statuses/update.json
+month=`date '+%m'`
+file=`cat /home/crukky/tweet_health_bot/month/${month}.txt`
+log="/home/crukky/tweet_health_bot/tweet_health_bot.log"
+state="
+${month}月の旬の食べ物
+
+${file}
+"
+
+echo "start" >> ${log}
+echo `date` >> ${log}
+
+sudo -u crukky twurl -d "status=${state}" /1.1/statuses/update.json &>> ${log}
+
+echo "end" >> ${log}
